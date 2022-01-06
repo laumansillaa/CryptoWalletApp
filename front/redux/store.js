7b7ee04ec61e151640435610ca1953c173bb7f7c
alexander <alex.r010114@gmail.com>
2022-01-06 09:23:11 -0500

    global state structure with redux

---
+++ front/redux/store.js
@@ -0,0 +1,8 @@
+import { applyMiddleware, createStore } from 'redux';
+
+import thunk from 'redux-thunk';
+import rootReducer  from "./reducer";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;

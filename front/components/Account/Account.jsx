02042dc61c6ea0b8f3daa40bc9312536ba774b9d
agudlc <87042363+agudlc@users.noreply.github.com>
2022-01-08 10:42:15 -0300

    Update Account.jsx

--- front/components/Account/Account.jsx
+++ front/components/Account/Account.jsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function Account() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Account Screen</Text>
+      <Text>Account Screen!</Text>
     </View>
   );
 }

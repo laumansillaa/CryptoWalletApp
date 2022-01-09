9f9d2a95cf1ea6b178c9ff052b5c2836bb9824f4
alexander <pedro.susa.reyes@unillanos.edu.co>
2022-01-09 12:45:56 -0500

    validate email,firstname,lastname,password,pin,ph

--- front/components/Register/Register.jsx
+++ front/components/Register/Register.jsx
@@ -1,72 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, } from 'react-native';
 
-import { extendTheme, NativeBaseProvider, Box, Divider,Stack, Input, Icon, FormControl,WarningOutlineIcon,Heading,Button } from 'native-base';
-import { MaterialIcons } from "@expo/vector-icons"
+import {  NativeBaseProvider, Stack, Input,  FormControl,WarningOutlineIcon,Heading,Button } from 'native-base';
+
 import { useState, useEffect } from 'react';
-import { validateEmail } from '../Utils/Utils';
+import { validateEmail, validateNumber, validatePassword, validateString, validatePin } from '../Utils/Utils';
 export default function Register() {
 
 
 const [state,setState] = useState({
-  name: "",
-  lastName: "",
+  firstname: "",
+  lastname: "",
   email:"",
   phone:"",
-  password:""
+  password:"",
+  pin:""
 
 })
 const [error, setError] = useState({
-  errorName: "",
-  errorLastName: "",
-  errorEmail:"",
-  errorPhone:"",
-  errorPassword:""
+  firstName: "",
+  lastName: "",
+  email:"",
+  phone:"",
+  password:"",
+  pin:""
 
 })
 
-function validateData (){
-  setError({...error, errorName: "",
-  errorLastName: "",
-  errorEmail:"",
-  errorPhone:"",
-  errorPassword:""})
+function validateData (arg){
 
-  let isValid = true
-  if(state.email?!validateEmail(state.email):false){
-    setError({...error, errorEmail: "Debe ingresar un email valido"})
-    isValid = false;
+
+  switch(arg){
+    case "firstname":
+     validateString(state.firstname)? setError({...error, firstName: ""}):setError({...error, firstName:"Please enter a valid firstname without special characters"});
+    break;
+    case "lastname":
+      validateString(state.lastname)? setError({...error, lastName: ""}):setError({...error, lastName:"Please enter a valid firstname without special characters"});
+    break;
+    case "email":
+      validateEmail(state.email)? setError({...error, email: ""}):setError({...error, email:"Please enter a valid email"});
+    break;
+    case "phone":
+      validateNumber(state.phone)? setError({...error, phone: ""}):setError({...error, phone:"Please just enter numbers"});
+    break;
+    case "password":
+      validatePassword(state.password)? setError({...error, password: ""}):setError({...error, password:"Please enter a 6 to 16 digit password with at least one number and one capital letter"});
+    break;
+    case "pin":
+      validatePin(state.pin)? setError({...error, pin: ""}):setError({...error, pin:"Please enter a 6-digit pin, only numbers are accepted"});
+    break;
   }
-  if(state.password?!(state.password.length === 6) :false){
-    setError({...error, errorPassword: "Debe ingresar una password valido"})
-    isValid = false;
-  }
-  
 
-  
-
-  return isValid;
 }
 
 
-useEffect(()=>{
-
-  validateData()
-  
-  
-},[state])
-
+useEffect(()=>{validateData("firstname")},[state.firstname])
+useEffect(()=>{validateData("lastname")},[state.lastname])
+useEffect(()=>{validateData("phone")},[state.phone])
+useEffect(()=>{validateData("password")},[state.password])
+useEffect(()=>{validateData("email")},[state.email])
+useEffect(()=>{validateData("pin")},[state.pin])
+ 
 
 
 const handleChange = (e, atr)=>{
 
-setState({...state, [atr]: e})
+setState({...state, [atr]: e})}
 
-
-}
 
 function handleSubmit(){
-validateData()
+    if(!error.firstName&&!error.lastName&&!error.email&&!error.phone&&!error.password&&!error.pin){
+    console.log("registro ok")
+
+  }
 
 }
 
@@ -80,9 +86,7 @@ validateData()
     w={{
       base: "100%",
       md: "25%",}}
-    style={styles.container}
-    
-  >
+    style={styles.container}>
 
 
           <Heading>Register </Heading>
@@ -97,21 +101,36 @@ validateData()
         md:"25%"}}
       >  
         
-        <Input variant="filled"  placeholder="Enter Name" onChangeText={(e)=>handleChange(e,"name")} />  
-     
-        <Input variant="filled"  placeholder="Enter Last Name" onChangeText={(e)=>handleChange(e,"lastName")}/>  
-        
-        <Input  variant="filled" placeholder="Enter Email" onChangeText={(e)=>handleChange(e,"email")} errorMessage={error.errorEmail} />
+        <Input variant="filled"   placeholder="Enter Name" onChangeText={(e)=>handleChange(e,"firstname")} />
         <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-          {error.errorEmail?error.errorEmail:""}
+          {error.firstName}
+        </FormControl.ErrorMessage>  
+     
+        <Input variant="filled"  placeholder="Enter Last Name" onChangeText={(e)=>handleChange(e,"lastname")}/> 
+        <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+          {error.lastName}
+        </FormControl.ErrorMessage>   
+        
+        <Input  variant="filled" placeholder="Enter Email" onChangeText={(e)=>handleChange(e,"email")} />
+        <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+          {error.email}
         </FormControl.ErrorMessage>  
         
-        <Input  variant="filled"  placeholder="Enter Phone" onChangeText={(e)=>handleChange(e,"phone")} />  
+        <Input  variant="filled"  placeholder="Enter Phone" onChangeText={(e)=>handleChange(e,"phone")} /> 
+        <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+          {error.phone}
+        </FormControl.ErrorMessage>   
     
         <Input variant="filled"  placeholder="Enter Password" onChangeText={(e)=>handleChange(e,"password")} password={true} secureTextEntry={true}
          />  
            <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-          {error.errorPassword?error.errorPassword:""}
+          {error.password}
+        </FormControl.ErrorMessage>  
+
+        <Input variant="filled"  placeholder="Enter 6 digit pin" onChangeText={(e)=>handleChange(e,"pin")} password={true} secureTextEntry={true}
+         />  
+           <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+          {error.pin}
         </FormControl.ErrorMessage>  
 
         <Button onPress={handleSubmit}>Next</Button>
--- front/components/Utils/Utils.js
+++ front/components/Utils/Utils.js
@@ -1,4 +1,37 @@
 export function validateEmail(email) {
+  if(!email) return true;
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email)
+  }
+
+  export function validateString(str){
+    if(!str) return true;
+    const re = /^[A-Za-z ]+$/
+    return re.test(str)
+  }
+  export function validateNumber(num){
+    if(!num)return true;
+    const re = /^[0-9]+$/
+    return re.test(num)
+
+  }
+  export function validatePassword(pass){
+    if(!pass) return true;
+ 
+    const re = /^(?=.*[0-9])(?=.*[A-Z])[A-Za-z0-9]{6,16}$/
+    return re.test(pass)
+
+  
+  }
+
+  export function validatePin(pin){
+    if(!pin) return true
+    
+    const re = /^[0-9]{6}$/
+  
+    return re.test(pin)
+
+
+ 
+
   }

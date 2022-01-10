74f38baa08a5c79065b9b3f09b36db432c3af72f
Micaela Reyes <reyessmicaela@gmail.com>
2022-01-09 22:00:56 -0300

    add header-user, add components

--- front/App.js
+++ front/App.js
@@ -3,23 +3,24 @@ import { Provider } from 'react-redux';
 
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import store from "./redux/store";
 import Home from './components/Home/Home';
-import Login from './components/Login/Login';
-import Register from "./components/Register/Register"
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Currencies from './components/Currencies/Currencies';
+import Account from './components/Account/Account';
 
-const Stack = createNativeStackNavigator();
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName='Login'>
-          <Stack.Screen name="Login" component={Login}/>
-          <Stack.Screen name="Register" component={Register}/>
-          <Stack.Screen name="Home" component={Home}/>
-        </Stack.Navigator>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={Home}/>
+          <Tab.Screen name="Currencies" component={Currencies}/>
+          <Tab.Screen name="Account" component={Account}/>
+        </Tab.Navigator>
       </NavigationContainer>
     </Provider>
   );
---
+++ front/components/Help/Help.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center' 
+  },
+});

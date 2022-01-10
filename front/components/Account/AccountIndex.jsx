4576a60a442328294a688ad10b3afed668f259d5
Micaela Reyes <reyessmicaela@gmail.com>
2022-01-10 20:32:55 -0300

    modified components

--- front/components/Account/AccountIndex.jsx
+++ front/components/Account/AccountIndex.jsx
@@ -8,19 +8,20 @@ import Security from '../Security/Security';
 import Settings from '../Settings/Settings';
 import Account from './Account';
 import EditDataUser from "../EditDataUser/EditDataUser";
+import HeaderUser from '../HeaderUser/HeaderUser'
 
 const Stack = createNativeStackNavigator();
 
 export default function AccountIndex() {
-    return (
-        <Stack.Navigator initialRouteName='Account'>
-        <Stack.Screen name="Account" component={Account} />
-        <Stack.Screen name="MyData" component={MyData}/>
-        <Stack.Screen name="MyTags" component={MyTags}/>
-        <Stack.Screen name="Security" component={Security}/>
-        <Stack.Screen name="Settings" component={Settings}/>
-        <Stack.Screen name="Help" component={Help}/>
-        <Stack.Screen name="EditDataUser" component={EditDataUser}/>
-      </Stack.Navigator>
-    );
-  }
+  return (
+    <Stack.Navigator initialRouteName='Account'>
+      <Stack.Screen name="AccountComponent" component={Account} options={{ headerTitle: () => <HeaderUser /> }} />
+      <Stack.Screen name="MyData" component={MyData} />
+      <Stack.Screen name="MyTags" component={MyTags} />
+      <Stack.Screen name="Security" component={Security} />
+      <Stack.Screen name="Settings" component={Settings} />
+      <Stack.Screen name="Help" component={Help} />
+      <Stack.Screen name="EditDataUser" component={EditDataUser} />
+    </Stack.Navigator>
+  );
+}

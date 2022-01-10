da3bc438892a86df1f3e633dbe4ea12548fa6db5
Micaela Reyes <reyessmicaela@gmail.com>
2022-01-10 12:34:36 -0300

    add menu components

--- front/components/Home/Home.jsx
+++ front/components/Home/Home.jsx
@@ -3,13 +3,14 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Currencies from '../Currencies/Currencies';
 import Account from '../Account/Account';
 import HeaderUser from '../HeaderUser/HeaderUser';
+import HeaderCurrencies from '../HeaderCurrencies/HeaderCurrencies'
 
 const Tab = createBottomTabNavigator();
 
 export default function Home() {
   return (
     <Tab.Navigator>
-        <Tab.Screen name="Currencies" component={Currencies}/>
+        <Tab.Screen name="Currencies" component={Currencies} options= {{headerTitle:()=><HeaderCurrencies/>}}/>
         <Tab.Screen name="Account" component={Account} options={{headerTitle:()=><HeaderUser/>}}/>
     </Tab.Navigator>
     

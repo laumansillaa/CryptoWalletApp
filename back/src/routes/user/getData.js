bcc183df179c7a66fae97b4d62f1e30877eb9d79
jralvarezwindey <jralvarezwindey@gmail.com>
2022-01-18 17:41:04 -0300

    Get user data route updated. Now we are sending also user's contacts and user's publics keys

--- back/src/routes/user/getData.js
+++ back/src/routes/user/getData.js
@@ -1,4 +1,27 @@
-module.exports = async function(req, res) {
+const { Contact, Key } = require("../../db.js").models;
+
+module.exports = async function(req, res, next) {
     console.log('---------- ROUTE USER GET DATA ----------')
-    return res.status(200).send(req.user);
+    try  {
+        const contacts = await Contact.findAll({
+            where: {
+                userId: req.user.id
+            }
+        });
+
+        const keys = await Key.findOne({
+            where: {
+                userId: req.user.id
+            }
+        });
+
+        return res.status(200).send({
+            ...req.user.dataValues,
+            contacts,
+            publicKeys: {
+                ethereum: keys.ethereum[0],
+                stellar: keys.stellar[0]
+            }
+        });
+    } catch(error) { next(error) }
 };

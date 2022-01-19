d1238497954fa7a052c321dfd865a5d9edf632b5
alexander <alex.r010114@gmail.com>
2022-01-19 02:40:13 -0500

    transation stellar ok

--- front/components/Home/components/Transfer.jsx
+++ front/components/Home/components/Transfer.jsx
@@ -16,13 +16,14 @@ import {
   VStack,
   ZStack,
   Modal,
-  FormControl
+  FormControl,
+  HStack
 
   
 } from 'native-base';
 
 import { Pressable} from 'react-native';
-
+import axios from 'axios';
 
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -30,42 +31,100 @@ import { useDispatch, useSelector } from 'react-redux';
 
 export default function Transfer({route, navigation}) {
       const {currency, amount} = route.params
+      const [disabledButton, setDisableButton] = useState(true)
       const [showModal, setShowModal] = useState(false)
-    
-      const [founds, setFounds] = useState("");
+      const blockChain = useSelector(state => state.blockChain);
+      const [urlBlockChain, setUrlBlockChain]= useState("");
+      const [founds, setFounds] = useState("0.00");
+      const [publicKey, setPublicKey] = useState("")
+      const [mes, setMes] = useState("")
+      React.useEffect(()=>{
+
+        if(blockChain === "stellar"){
+          setUrlBlockChain("stellar")
+        }
+        else if(blockChain === "ethereum"){
+  
+          setUrlBlockChain("ethereum");
+        }
+  
+  
+      },[blockChain])
+
+
+
+async function transferUser (){
+  try {
+    setMes("loading...")
+    const response = await axios({
+      method: "post",
+      data: {
+        transferCurrency: currency,
+        transferAmount:founds,
+        publicKey: publicKey
+      },
+      withCredentials: true,
+      url: `http://${IP_HOST}:3001/operation/${urlBlockChain}/transfer`,
+    });
+
+    setMes(response.data)
+    setTimeout(()=>navigation.navigate("CurrenciesIndex"),1000)
+
+  } catch (error) {
+    setMes("Failed Transfer")
+    console.error(error);
+  } 
+
+
+}
+
+
+
+
+
+
+
     return (
       <>    
-     
+      {/* Componente amount y button go back */}
            <Box
           mt="50px"
           py="1"
-          
-          rounded="md"
+          mb="5"
+          rounded="xl"
           alignSelf="center"
           width={375}
           maxWidth="100%"
+          bg="#FFC902"
+          
          
           >
 
-          <Stack direction="row" alignItems="center">
+          <Stack direction="row" alignItems="center" rounded="md">
           <Pressable   onPress={()=> navigation.goBack()}>
           <ChevronLeftIcon color="darkBlue.900" size="9"/>
           </Pressable>
           <VStack>
-          <Text ml="70px" fontSize="xl" color="darkBlue.900" fontWeight="bold" >Amount available </Text> 
+          <Text ml="70px" fontSize="xl" color="black" fontWeight="bold" >Amount available </Text> 
              
           </VStack>
              
           </Stack>
+          <VStack alignSelf="center">
+          
+          <Text color="#ffffff" ml="24px" fontWeight="bold" mt="-5" fontSize="6xl"> {amount} </Text>
+          <Text ml="220px" mt="-5" fontSize="xl"  color="darkBlue.800" fontWeight="bold" >{currency} </Text> 
+          </VStack>
+          
           </Box>
-          <ZStack>
-              <Text ml="300px" fontSize="xl" color="darkBlue.900" fontWeight="bold" >{currency} </Text> 
+         {/* Currency and amount */}
+         
           <Box alignSelf="center" alignItems="center" >
           
-          <Text color="darkBlue.900" fontWeight="bold" fontSize="6xl"> {amount} </Text>
-  {/*         <Text color="darkBlue.900" fontWeight="bold" fontSize="6xl"> ${stateToken?.price} </Text> */}
-          <Box
-             bg="darkBlue.900"
+
+
+          {/* <Box
+             bg="black"
              
              
             shadow={9}
@@ -77,22 +136,82 @@ export default function Transfer({route, navigation}) {
              maxWidth="100%"
              maxHeight="100%"
           >
-            <Text color="#ffffff" mt="2" fontWeight="bold" fontSize="lg" pb="1">
-             Data :
-            </Text>
-      </Box>
-     
            
-          </Box>
-          </ZStack>
+          </Box> */}
          
+          <Box
+          mt="20px"
+          py="1"
+          mb="5"
+          alignItems="center"
+          rounded="xl"
+          alignSelf="center"
+          width={375}
+          maxWidth="100%"
+          bg="#CF2E2E"
+          
+          
+         
+          >
+
+          <Text color="#ffffff" mt="2" fontWeight="bold" fontSize="lg" pb="1">
+            Amount to transfer from {currency}:
+            </Text>
+          <Text color="#ffffff" fontWeight="bold" fontSize="6xl"> {founds} </Text>
+          <Text color="#ffffff" mt="2" fontWeight="bold" fontSize="lg" pb="1">
+              To the wallet:
+            </Text>
+            <Box >
+            <Text color="#ffffff" mt="2" fontWeight="bold" fontSize="sm" pb="1">
+             {publicKey}
+            </Text>
+            </Box>
+            
+      </Box>
+
+      <Box
+          mt="20px"
+          py="1"
+          mb="5"
+          alignItems="center"
+          rounded="xl"
+          alignSelf="center"
+          width={375}
+          maxWidth="100%"
+          bg="darkBlue.900"
+           >
+
+          <Text color="#ffffff" mt="2" fontWeight="bold" fontSize="lg" pb="1">
+            Your new {currency} amount will be: :
+            </Text>
+          <Text color="#ffffff" fontWeight="bold" fontSize="6xl"> {(parseFloat(amount)- parseFloat(founds).toFixed(4)) } </Text>
+      
+      </Box>
+      {(mes)?mes:""}
+      <Text ml="70px" fontSize="xl" color="black" fontWeight="bold" > {(mes)?mes:""} </Text> 
+      <HStack alignSelf="center">
+      <Button rounded="lg" px="7" py="1" bg="darkBlue.900"onPress={() => setShowModal(true)}>
+        <Text color="#ffffff" fontSize="4xl" fontWeight="bold">Mont</Text></Button>
+        <Button ml="2"rounded="lg" px="7" bg="black" py="1" isDisabled={disabledButton} onPress={() => transferUser()}>
+        <Text color="#ffffff" fontSize="4xl" fontWeight="bold">Confirm</Text></Button>
+      </HStack>
+
+      {(mes)?mes:""}
+
+          </Box>
+      
+          
+        
+         
+
           <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
           <Modal.Content maxWidth="500px">
             <Modal.CloseButton />
-            <Modal.Header>Add founds</Modal.Header>
+            <Modal.Header>Amount to transfer</Modal.Header>
             <Modal.Body>
               <FormControl>
-                <FormControl.Label>how much money do you want to add?</FormControl.Label>
+                <FormControl.Label>How much {currency} do you want to transfer?</FormControl.Label>
+                <VStack alignItems="center">
                 <InputGroup
                   w={{
                     base: "70%",
@@ -108,9 +227,19 @@ export default function Transfer({route, navigation}) {
                     placeholder="Amount"
                     onChangeText={setFounds}
                   />
+                  
 
                 </InputGroup>
-
+                <Input
+                  mt="5"
+                    w={{
+                      base: "70%",
+                      md: "100%",
+                    }}
+                    placeholder="Public Key.."
+                    onChangeText={setPublicKey}
+                  />
+                  </VStack>
               </FormControl>
 
             </Modal.Body>
@@ -121,6 +250,7 @@ export default function Transfer({route, navigation}) {
                   colorScheme="blueGray"
                   onPress={() => {
                     setShowModal(false)
+                    
                   }}
                 >
                   Cancel
@@ -128,6 +258,7 @@ export default function Transfer({route, navigation}) {
                 <Button
                   onPress={() => {
                    setShowModal(false)
+                   setDisableButton(false)
 
                    
                   }}

ba4cf7787d6e0f8a9c00c9ba239cd9db1cea00d5
agudlc <87042363+agudlc@users.noreply.github.com>
2022-01-19 09:00:41 -0300

    safe commit

--- back/src/db.js
+++ back/src/db.js
@@ -31,39 +31,36 @@ const sequelize = process.env.NODE_ENV === "production"
       native: false
     });
 
-<<<<<<< HEAD
 // Connect models to sequelize.
 
 // const RecipeModelCreator = require('./models/Recipe.js')
 // const DietModelCreator = require('./models/Diet')
 // RecipeModelCreator(sequelize);
 // DietModelCreator(sequelize);
-const UserModel = require ('./models/User.js')
-UserModel(sequelize);
-const Transaction = require ('./models/Transaction.js')
-Transaction(sequelize);
-const AccountMovement = require ('./models/AccountMovements.js')
-AccountMovement(sequelize);
+// const UserModel = require ('./models/User.js')
+// UserModel(sequelize);
+// const Transaction = require ('./models/Transaction.js')
+// Transaction(sequelize);
+// const AccountMovement = require ('./models/AccountMovements.js')
+// AccountMovement(sequelize);
 
 
-// const { User, Transactions } = sequelize.models;
-require('./models')(sequelize)
+// // const { User, Transactions } = sequelize.models;
+// require('./models')(sequelize)
 
-// Associations.
-// const { Recipe, Diet } = sequelize.models;
-// Recipe.belongsToMany(Diet, {as: 'diets', through: 'RecipesDiets', foreignKey: 'recipeId'});
-// Diet.belongsToMany(Recipe, {as: 'recipes', through: 'RecipesDiets', foreignKey: 'dietId'});
+// // Associations.
+// // const { Recipe, Diet } = sequelize.models;
+// // Recipe.belongsToMany(Diet, {as: 'diets', through: 'RecipesDiets', foreignKey: 'recipeId'});
+// // Diet.belongsToMany(Recipe, {as: 'recipes', through: 'RecipesDiets', foreignKey: 'dietId'});
 
-// Transactions.belongsTo(User, {foreignKey: "publicKey"});
-// User.hasMany(Transactions, {foreignKey: "from"});
+// // Transactions.belongsTo(User, {foreignKey: "publicKey"});
+// // User.hasMany(Transactions, {foreignKey: "from"});
 
-const { User, Operation, Key } = sequelize.models;
-Operation.belongsToMany(User, {as: 'users', through: 'UserOperation', foreignKey: 'operationId'});
-User.belongsToMany(Operation, { as: 'operations', through: 'UserOperation', foreignKey: 'userId'})
-User.hasOne(Key, {as: 'key', foreignKey: 'user'})
-=======
+// const { User, Operation, Key } = sequelize.models;
+// Operation.belongsToMany(User, {as: 'users', through: 'UserOperation', foreignKey: 'operationId'});
+// User.belongsToMany(Operation, { as: 'operations', through: 'UserOperation', foreignKey: 'userId'})
+// User.hasOne(Key, {as: 'key', foreignKey: 'user'})
 // Connection and association of data base models.
 require("./models")(sequelize)
->>>>>>> e5e4af06c74bacbee4f5ea65d6f993fafa88d51b
 
 module.exports = sequelize;

2497bcab068f976da3f596ebe431929763a90af9
Juan I. Quiroga <juaniquiroga13@gmail.com>
2022-01-24 10:43:48 -0300

    currency

--- back/src/routes/balance/data.js
+++ back/src/routes/balance/data.js
@@ -12,27 +12,27 @@ module.exports = async function(req, res, next) {
         const keys = await Key.findOne({ where: { userId: req.user.id } });
         const prices = await binance.futuresPrices();
 
-        const ethereumEther = (await web3.eth.getBalance(keys.ethereum[0]))/10**18;
-        const ethereumCurrencies = [{ currency: "ETH", amount: ethereumEther }] ;
-        await Promise.all(["HNR", "BTC", "USDT", "BNB", "ADA", "SOL"].map(async currency => {
-            const tokenContract = await require("../../solidity")(currency);
-            const amount = (await tokenContract.methods.balanceOf(keys.ethereum[0]).call()) / 10**4;
-            ethereumCurrencies.push({
-                currency,
-                amount, 
-            });
-        }));
-        let ethereumCrypto = 0;
-        let ethereumStaking = 0;
-        for (let i = 0; i < ethereumCurrencies.length; i++) {
-            if (ethereumCurrencies[i].currency === "USDT") {
-                ethereumCrypto += ethereumCurrencies[i].amount;
-            } else if (ethereumCurrencies[i].currency === "HNR") {
-                ethereumCrypto += ethereumCurrencies[i].amount * 4000;
-            } else {
-                ethereumCrypto += ethereumCurrencies[i].amount * prices[`${ethereumCurrencies[i].currency}USDT`];
-            }
-        }
+        // const ethereumEther = (await web3.eth.getBalance(keys.ethereum[0]))/10**18;
+        // const ethereumCurrencies = [{ currency: "ETH", amount: ethereumEther }] ;
+        // await Promise.all(["HNR", "BTC", "USDT", "BNB", "ADA", "SOL"].map(async currency => {
+        //     const tokenContract = await require("../../solidity")(currency);
+        //     const amount = (await tokenContract.methods.balanceOf(keys.ethereum[0]).call()) / 10**4;
+        //     ethereumCurrencies.push({
+        //         currency,
+        //         amount, 
+        //     });
+        // }));
+        // let ethereumCrypto = 0;
+        // let ethereumStaking = 0;
+        // for (let i = 0; i < ethereumCurrencies.length; i++) {
+        //     if (ethereumCurrencies[i].currency === "USDT") {
+        //         ethereumCrypto += ethereumCurrencies[i].amount;
+        //     } else if (ethereumCurrencies[i].currency === "HNR") {
+        //         ethereumCrypto += ethereumCurrencies[i].amount * 4000;
+        //     } else {
+        //         ethereumCrypto += ethereumCurrencies[i].amount * prices[`${ethereumCurrencies[i].currency}USDT`];
+        //     }
+        // }
 
         const stellarAccount = await server.loadAccount(keys.stellar[0]);
         const stellarCurrencies = stellarAccount.balances
@@ -54,11 +54,11 @@ module.exports = async function(req, res, next) {
         const user = await User.findOne({ where: { id: req.user.id } })
 
         return res.status(200).send({
-            ethereum: {
-                cryptoBalance: ethereumCrypto.toString(),
-                stakingBalance: ethereumStaking.toString(),
-                currencies: ethereumCurrencies
-            },
+            // ethereum: {
+            //     cryptoBalance: ethereumCrypto.toString(),
+            //     stakingBalance: ethereumStaking.toString(),
+            //     currencies: ethereumCurrencies
+            // },
             stellar: {
                 cryptoBalance: stellarCrypto.toString(),
                 stakingBalance: stellarStaking.toString(),
--- front/components/HeaderCurrencies/Chart/Chart.jsx
+++ front/components/HeaderCurrencies/Chart/Chart.jsx
@@ -7,7 +7,7 @@ import { LineChart } from "react-native-chart-kit";
 export default function SegmentChartGrandient() {
     const chartInfo = useSelector(state=> state.monthPrices);
     return (
-      
+        <View>
             <LineChart
                 data={{
                     labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
@@ -49,7 +49,7 @@ export default function SegmentChartGrandient() {
                     borderRadius: 16
                 }}
             />
-       
+       </View>
     )
 }
 

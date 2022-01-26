dd6547c639c7e2b7151b23cd24bf1012abdce957
agudlc <87042363+agudlc@users.noreply.github.com>
2022-01-26 08:54:57 -0300

    conflict middlewares

--- front/components/PasswordRecovery/PasswordRecovery.jsx
+++ front/components/PasswordRecovery/PasswordRecovery.jsx
@@ -14,6 +14,7 @@ export default function PasswordRecovery({ navigation }) {
     const [error, setError] = useState({
         email:""
         });
+    const [emailSent, setEmailSent] = useState(false);
     const [message, setMessage] = useState("");
 
     
@@ -38,7 +39,7 @@ export default function PasswordRecovery({ navigation }) {
                 withCredentials: true,
                 url: `http://${IP_HOST}:3001/password/tokenrequest`,
               });
-            setMessage("Token generated")
+            setEmailSent(true);
         } catch (e) {console.log(e)};
 
     }
@@ -49,14 +50,27 @@ export default function PasswordRecovery({ navigation }) {
             <Center>
                 <FormControl >
                 <Stack>
-                    <Text>Put your email for generate a Token</Text>
-                    <Input placeholder="email" value={email} onChangeText={setEmail} InputLeftElement={
-                        <Icon as={<MaterialIcons name="person" />} size={5} ml="2" color="muted.400" />} 
-                        color='coolGray.900' backgroundColor= 'darkBlue.50' size= "lg" borderRadius= "4px"/>
+                    <Text fontSize="xl">Put your email</Text>
+
+                    <Divider my="1" bg='#ecfeff' />
+
+                    <Input placeholder="email" value={email} onChangeText={setEmail} 
+                    InputLeftElement={<Icon as={<MaterialIcons name="person" />} size={5} ml="2" color="muted.400" />} 
+                    color='coolGray.900' 
+                    backgroundColor= '#e4e4e7' 
+                    size= "lg" borderRadius= "4px"
+                    borderColor= "#dark.900" 
+                    borderWidth="2"/>
                     {error.email !== "Please enter a valid email" && email !== "" ? 
                     
-                    <Button onPress={generateToken} size="sm" backgroundColor= 'darkBlue.600' borderRadius= "4px"
-                     _text={{fontSize:"md"}} borderColor= "darkBlue.50" borderWidth="1">Generate a Token</Button> :
+                    <Button onPress={generateToken} mt= "20px" size="sm" 
+                    borderRadius= "4px" 
+                    bg= 'theme.50' 
+                    color= 'theme.100'
+                    _text={{fontSize:"md"}} 
+                    borderColor= "darkBlue.50" 
+                    borderWidth="1">Generate a Token</Button> :
+
                     <FormControl.HelperText leftIcon={<WarningOutlineIcon size="md" />}>
                     {error.email} 
                     </FormControl.HelperText> }
@@ -66,11 +80,23 @@ export default function PasswordRecovery({ navigation }) {
                     <Divider my="2" bg='#ecfeff' /> 
                     <Button onPress={() => navigation.navigate("PasswordReset")} size="sm" 
                     backgroundColor= 'darkBlue.600' borderRadius= "4px"  _text={{fontSize:"md"}} 
-                    borderColor= "darkBlue.50" borderWidth="1">Reset Password</Button>
+                    borderColor= "darkBlue.50" borderWidth="1">Reset Password</Button>                 
                 </Stack>
-                <Button onPress={() => navigation.navigate("Login")} size="sm" backgroundColor= 'darkBlue.600' borderRadius= "4px"
-                leftIcon= {<Icon as={<AntDesign name="back" size={5} color="black" />} />} h="9" width= "250" 
-                 _text={{fontSize:"md"}} borderColor= "darkBlue.50" borderWidth="1">Go to back</Button>
+
+                <Divider my="4" bg='#ecfeff' />
+                
+                <Button onPress={() => navigation.navigate("Login")} 
+                mt= "40px" 
+                size="sm"  
+                borderRadius= "4px"
+                leftIcon= {<Icon as={<AntDesign name="back" size={5} color="black" />} />} 
+                h="9" w= "320" 
+                _text={{fontSize:"md"}} 
+                borderColor= "darkBlue.50" 
+                borderWidth="1" 
+                fontSize="xl" 
+                bg= 'theme.50' 
+                color= 'theme.100' >Go to back</Button>
                 </FormControl>
             </Center>
         </Box>
@@ -83,7 +109,7 @@ const windowsHeight = Dimensions.get("window").height;
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#000e21',
+      backgroundColor: '#18181b',
       
       alignItems: 'center',
       justifyContent: 'center',
--- front/components/PasswordRecovery/PasswordReset.jsx
+++ front/components/PasswordRecovery/PasswordReset.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Center,  Divider, FormControl, Icon, Input, Stack, Text } from "native-base";
+import { Button, Center, Container, FormControl, Icon, Input, Stack, Text, WarningOutlineIcon, Box, Divider } from "native-base";
 import React, { useEffect, useState } from "react";
 import { StyleSheet, Dimensions } from "react-native";
 import { AntDesign } from "@expo/vector-icons"
@@ -6,7 +6,8 @@ import { validatePassword } from "../Utils/Utils";
 import {IP_HOST} from "@env";
 import axios from "axios";
 
-export default function PasswordReset() {
+
+export default function PasswordReset({navigation}) {
 
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
@@ -54,29 +55,70 @@ export default function PasswordReset() {
             <Center>
                 <FormControl>
                 <Stack>
-                    <Text>Insert the Token</Text>
+                <Text>Insert the Token</Text>
                     <Input placeholder="Token" value={tokenPassword} onChangeText={setTokenPassword} color='coolGray.900' 
                     backgroundColor= 'darkBlue.50' size= "lg" borderRadius= "4px" InputLeftElement={
                     <Icon as={<AntDesign name="lock1" size={24} color="black" />} size={5} ml="2" color="muted.400" />} />
-                    <Text>Put your new password</Text>
-                    <Input placeholder="password"  value={password} onChangeText={setPassword} type="password"
-                    color='coolGray.900' backgroundColor= 'darkBlue.50' size= "lg" borderRadius= "4px"/>
+                    <Text fontSize="xl">Put your new password</Text>
+
+                    <Input placeholder="Password"  value={password} onChangeText={setPassword} 
+                    type="password"
+                    color='coolGray.900' 
+                    backgroundColor= '#e4e4e7' 
+                    size= "lg" 
+                    borderRadius= "4px" 
+                    w= "250px"
+                    InputLeftElement={<Icon as={<AntDesign name="key" size={24} color="black" />} 
+                    size={5} ml="2" 
+                    color="muted.400" />}
+                    borderColor= "#dark.900" 
+                    borderWidth="2"/>
+
                     <FormControl.HelperText >
                     {error.password}
-                    </FormControl.HelperText>  
-                    <Input placeholder="confirm your password"  value={confirmPassword} onChangeText={setConfirmPassword} type="password"
-                    color='coolGray.900' backgroundColor= 'darkBlue.50' size= "lg" borderRadius= "4px"/>
+                    </FormControl.HelperText>
+
+                    <Input placeholder="Confirm your password"  value={confirmPassword} onChangeText={setConfirmPassword} 
+                    type="password"
+                    color='coolGray.900' 
+                    backgroundColor= '#e4e4e7' 
+                    size= "lg" 
+                    borderRadius= "4px" 
+                    w= "250px"
+                    InputLeftElement={<Icon as={<AntDesign name="key" size={24} color="black" />}size={5} ml="2" color="muted.400" />}
+                    borderColor= "#dark.900" 
+                    borderWidth="2"/>
+
                     <FormControl.HelperText>
                     {error.confirmPassword}
                     </FormControl.HelperText>
-                    { password === confirmPassword ? <Text>the passwords match</Text> : 
-                    <Text>the passwords must match</Text>}
-                    <Divider my="2" bg='#ecfeff' />
-                    { error.password === "" ? <Button onPress={resetPassword} size="sm" backgroundColor= 'darkBlue.600'
-                     borderRadius= "4px"  _text={{fontSize:"md"}} borderColor= "darkBlue.50" borderWidth="1">Reset the password</Button> : 
-                    <Text>The password is not right</Text> } 
+
+                    { password === confirmPassword ? <Text fontSize="xl">The passwords match</Text> : 
+
+                    <Text fontSize="xl">The passwords must match</Text>}
+                    { error.password === "" ? 
+
+                    <Button onPress={resetPassword} 
+                    size="sm" 
+                    borderRadius= "4px" 
+                    bg= 'theme.50' 
+                    color= 'theme.100'
+                     _text={{fontSize:"md"}} 
+                     borderColor= "darkBlue.50" 
+                     borderWidth="1">Reset the password</Button> : 
+
+                    <Text fontSize="xl">The password is not right</Text> } 
+
                     <Text>{message}</Text>
-                                        
+
+                    <Divider my="4" bg='#ecfeff' />
+
+                    <Button  onPress={() => navigation.navigate("Login") } 
+                    _text={{fontSize:"md"}}
+                    borderColor= "darkBlue.50" 
+                    borderWidth="1" 
+                    bg= 'theme.50' 
+                    color= 'theme.100'>Go to back</Button>                                        
                 </Stack>
                 </FormControl>
             </Center>
@@ -91,11 +133,11 @@ const windowsHeight = Dimensions.get("window").height;
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#000e21',
+      backgroundColor: '#18181b',
       
       alignItems: 'center',
       justifyContent: 'center',
-      width: '100%'
+      
      
     },
   });
